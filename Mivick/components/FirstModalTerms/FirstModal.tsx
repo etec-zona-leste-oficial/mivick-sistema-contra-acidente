623fac72fec502a24b63bf592f25a4fb481e7fdb
BernardoVxexra <bernardo.santos13@etec.sp.gov.br>
2025-11-21 21:09:37 -0300

    top

--- Mivick/components/FirstModalTerms/FirstModal.tsx
+++ Mivick/components/FirstModalTerms/FirstModal.tsx
@@ -1,1 +1,4 @@
-import React from 'react';
+import { Ionicons } from "@expo/vector-icons";
+import React, { useEffect, useRef } from "react";
+import { Animated, Text, TouchableOpacity, View } from "react-native";
+import Modal from "react-native-modal";
